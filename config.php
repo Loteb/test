dd1dbc629a71739203fdbf890588e0a4b1d555a5
Loteb <kiril228332@mail.ru>
2018-07-20 16:22:20 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/Users/user/Desktop/OpenServer/domains/localhost/catalog/');
+define('DIR_SYSTEM', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/');
+define('DIR_IMAGE', 'C:/Users/user/Desktop/OpenServer/domains/localhost/image/');
+define('DIR_LANGUAGE', 'C:/Users/user/Desktop/OpenServer/domains/localhost/catalog/language/');
+define('DIR_TEMPLATE', 'C:/Users/user/Desktop/OpenServer/domains/localhost/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/config/');
+define('DIR_CACHE', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/storage/download/');
+define('DIR_LOGS', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/Users/user/Desktop/OpenServer/domains/localhost/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
